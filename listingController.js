e3f24d9af9e5afea1b1de1ea9934cbd60a216cbb
Liam Wood <liamvwood@ufl.edu>
2018-09-09 19:03:49 -0400

    changed collapse toggle to just show

--- listingController.js
+++ listingController.js
@@ -2,7 +2,6 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
-    $scope.isCollapsed = true;
 
     /* 
       Implement these functions in the controller to make your application function 
@@ -10,7 +9,6 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
      */
     $scope.addListing = function() {
       if ($scope.entry !== undefined) {
-        console.log("New listing added");
         $scope.listings.push($scope.entry);
         $scope.listings.sort(function(a,b){
           return a.code.localeCompare(b.code);
@@ -20,19 +18,12 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
     };
 
     $scope.deleteListing = function(index) {
-      console.log(`Debug: delete ${index}`);
-      $scope.isCollapsed = true;
       $scope.listings.splice(index,1);
       $scope.detailedInfo = undefined;
     };
 
     $scope.showDetails = function(index) {
-      // console.log(`Debug: showDetails ${index}`);
-      if ($scope.isCollapsed === true) {
-        console.log("Debug: 1")
-        angular.element('#moreInfo').collapse("toggle");
-        $scope.isCollapsed = false;
-      }
+      angular.element('#moreInfo').collapse("show");
       $scope.detailedInfo = $scope.listings[index];
     };
   }
